3bcfe30423e9ba7da1e0558c99f9e79cd47da278
Rb-112594 <reboureslan@gmail.com>
2025-02-13 18:41:21 +0200

    established ticket prices according to age using if statement

--- ticket-price-calculator.py
+++ ticket-price-calculator.py
@@ -1,1 +1,10 @@
 age = int(input("Enter you age: "))
+if age <= 4:
+    print("The ticket price is $0.")
+elif age <= 12:
+    print("The ticket price is $5.")
+elif age <= 18:
+    print("The ticket price is $10.")
+else:
+    print("The ticket price is $15.")
+    
