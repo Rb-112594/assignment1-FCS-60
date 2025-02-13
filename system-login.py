b4da452fc1f3f328b57817f0184011664b27b2d8
Rb-112594 <reboureslan@gmail.com>
2025-02-13 20:04:12 +0200

    Checked provided username and password to grant or deny access

--- system-login.py
+++ system-login.py
@@ -1,3 +1,7 @@
 username = input("Enter Username: ")
 password = input("Enter Password: ")
 
+if username == "admin" and password == "1234" :
+    print("Access granted.")
+else:
+    print("Access Denied.")
